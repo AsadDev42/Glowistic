e471c567f23846cbe4028e735737f882725711a1
Asad Farooq <asadge22v@gmail.com>
2026-09-22 07:49:20 -0700

    fix(deployment): resolve Vercel static asset routing and move server.js to scripts

--- start-server.bat
+++ start-server.bat
@@ -1,7 +1,7 @@
-﻿@echo off
+@echo off
 title GLOWISTIC Local Storefront Server
 cd /d "%~dp0"
 echo Starting GLOWISTIC Local Web Server...
 start "" "http://localhost:3000"
-node server.js
+node scripts\server.js
 pause
